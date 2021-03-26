d7eed2d657635ea9eaf44cad6f51e0508839df1d
xouavang <xouavaj@gmail.com>
2021-03-25 21:22:05 -0700

    ComparerBackups

---
+++ CompareBackups/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CompareBackups
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Models.BackupFile OldBackupFile { get; set; } = new Models.BackupFile();
+        public Models.BackupFile NewBackupFile { get; set; } = new Models.BackupFile();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void ChooseFile_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var backupFile = ((Button)sender).Tag as Models.BackupFile;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                backupFile.Name = openFileDialog.SafeFileName;
+                backupFile.Path = openFileDialog.FileName;
+                backupFile.SplitHashAndFileName();
+            }
+        }
+
+        private void Compare_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var oldNotInNew = new Dictionary<string, string>();
+
+            foreach (var item in OldBackupFile.Contents)
+            {
+                if (!NewBackupFile.Contents.Remove(item.Key))
+                {
+                    oldNotInNew.Add(item.Key, item.Value);
+                }
+            }
+
+            SaveToFile("OldNotInNew.txt", oldNotInNew);
+            SaveToFile("NewNotInOld.txt", NewBackupFile.Contents);
+
+            MessageBox.Show("Compare Complete");
+
+            ClearSelectedFiles();
+        }
+
+        /// <summary>
+        /// Clears the selected files from the user interface.
+        /// </summary>
+        private void ClearSelectedFiles()
+        {
+            OldBackupFile.Clear();
+            NewBackupFile.Clear();
+        }
+
+        /// <summary>
+        /// Saves the <see cref="Dictionary{TKey, TValue}"/> to a text file.
+        /// </summary>
+        /// <param name="fileName">The file name</param>
+        /// <param name="dict">The key value pair to be written to file</param>
+        private void SaveToFile(string fileName, Dictionary<string, string> dict)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                foreach (var entry in dict)
+                {
+                    writer.WriteLine("{0} {1}", entry.Key, entry.Value);
+                }
+            }
+        }
+    }
+}
---
+++ CompareBackups/Models/BackupFile.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+
+namespace CompareBackups.Models
+{
+    public class BackupFile : INotifyPropertyChanged
+    {
+        private const int SHA1_CHAR_COUNT = 40;
+
+        private string _name;
+        private string _path;
+
+        /// <summary>
+        /// Gets or sets the backup file's name.
+        /// </summary>
+        public string Name
+        {
+            get => _name; set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the backup file's full file path.
+        /// </summary>
+        public string Path
+        {
+            get => _path; set
+            {
+                _path = value;
+                OnPropertyChanged("Path");
+            }
+        }
+
+        /// <summary>
+        /// Gets the collection of keys and values, representing the SHA-1 Hash and File Name, respectively.
+        /// </summary>
+        public Dictionary<string, string> Contents { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        /// <summary>
+        /// Read each line in the text file and splits the context into their SHA-1 Hash and file name.
+        /// </summary>
+        public void SplitHashAndFileName()
+        {
+            Contents = new Dictionary<string, string>();
+
+            using (StreamReader sr = File.OpenText(Path))
+            {
+                string s = string.Empty;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    string[] subs = s.Split(null, 2);
+                    if (subs.Length > 1 && subs[0].Length == SHA1_CHAR_COUNT)
+                    {
+                        if (!Contents.ContainsKey(subs[0]))
+                        {
+                            Contents.Add(subs[0], subs[1]);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clears the object properties.
+        /// </summary>
+        public void Clear()
+        {
+            Name = string.Empty;
+            Path = string.Empty;
+            Contents = new Dictionary<string, string>();
+        }
+    }
+}
